8990f4346ca93378ff1134cd0c7bb9275545b413
Burhan Arslan <burhanarslanbas@gmail.com>
2026-07-02 16:23:58 +0300

    feat: complete debugging topic with console.log and debugger

---
+++ 6-debugging/src/App.jsx
@@ -0,0 +1,42 @@
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [vize1, setVize1] = useState(0);
+  const [vize2, setVize2] = useState(0);
+
+  const ortBul = () => {
+    debugger
+    const ort = topla() / 2;
+    yazdir(ort);
+  }
+
+  const topla = () => {
+    const toplam = vize1 + vize2;
+    debugger
+    return toplam;
+  }
+
+  const yazdir = (sonuc) => {
+    debugger
+    console.log('Ortalama Sonucu: ' + sonuc)
+  }
+  return (
+    <>
+      <div>
+        <input type="number" value={vize1} onChange={(e) => setVize1(Number(e.target.value))} />
+      </div>
+      <div>
+        <input type="number" value={vize2} onChange={(e) => setVize2(Number(e.target.value))} />
+      </div>
+      <div>
+        <button onClick={ortBul}>Ortalama Bul</button>
+      </div>
+    </>
+  )
+}
+
+export default App
